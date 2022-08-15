a48a67e9f6f62ac8669d588aea65190b92fe21ed
BreeChou <choujhen279@gmail.com>
2022-08-16 07:50:48 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,14 @@
-console.log('Hello!');
+// console.log('Hello!');
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+// $(document).ready(() => {
+//   console.log('HesSchool Hello!');
+// });
+
+$('.ham-icon').click(function(e) {
+  e.preventDefault();
+  $('.ham-dropdown').toggleClass('active')
+})
+
+
+//點選.ham-icon會觸發funtion執行e.preventDefault();>>取消預設行為
+//使用toggleClass讓.dropdown元素可以增加或取消active
